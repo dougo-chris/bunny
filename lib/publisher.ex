3a92439a13152116f488f266563cb1de42463a23
Chris Douglas <dougo.chris@gmail.com>
2018-11-30 09:03:58 +1100

    retry the request

--- lib/publisher.ex
+++ lib/publisher.ex
@@ -9,4 +9,14 @@ defmodule Linklab.Bunny.Publisher do
         AMQP.Basic.publish(channel, exchange, "", payload, options)
     end)
   end
+
+  @spec retry(atom, any, list) :: :ok | AMQP.Basic.error
+  def retry(name, payload, options \\ []) do
+    Linklab.Bunny.Pool.with_channel(name, fn
+      channel, %{exchange: exchange, topic: topic} ->
+        AMQP.Basic.publish(channel, exchange, "#{topic}-retry", payload, options)
+      channel, %{exchange: exchange} ->
+        AMQP.Basic.publish(channel, exchange, "retry", payload, options)
+    end)
+  end
 end
