fd738e51391619b93df7ec7a736a1277885754c7
Chris Douglas <dougo.chris@gmail.com>
2019-02-10 09:32:51 +1100

    bulk upgrade

--- mix.exs
+++ mix.exs
@@ -20,8 +20,8 @@ defmodule Linklab.Bunny.Mixfile do
     ]
   end
 
-  defp elixirc_paths(:test), do: ["lib", "priv", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
+  defp elixirc_paths(:test), do: ["lib", "task", "priv", "test/support"]
+  defp elixirc_paths(_), do: ["lib", "task"]
 
   defp deps do
     [
