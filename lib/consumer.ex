b13e68e0a1518ae71e5a0b8c03a2585f9820fa2c
Chris Douglas <dougo.chris@gmail.com>
2019-06-03 17:40:33 +1000

    basic messages

--- lib/consumer.ex
+++ lib/consumer.ex
@@ -4,12 +4,12 @@ defmodule Linklab.Bunny.Consumer do
   alias AMQP.Basic, as: AMQPBasic
   alias Linklab.Bunny.Pool, as: BunnyPool
 
-  @callback setup(AMQP.Channel.t(), map) :: :ok | no_return
-  @callback basic_deliver(AMQP.Channel.t(), any, map) :: :ok | no_return
-  @callback basic_consume_ok(AMQP.Channel.t(), map) :: :ok | no_return
-  @callback basic_cancel(AMQP.Channel.t(), map) :: :ok | no_return
-  @callback basic_cancel_ok(AMQP.Channel.t(), map) :: :ok | no_return
-  @callback basic_return(AMQP.Channel.t(), any, map) :: :ok | no_return
+  @callback setup(channel:: AMQP.Channel.t(), config:: map) :: :ok | no_return
+  @callback basic_deliver(channel:: AMQP.Channel.t(), payload :: any, meta :: map) :: :ok | no_return
+  @callback basic_return(channel:: AMQP.Channel.t(), payload :: any, meta :: map) :: :ok | no_return
+  @callback basic_consume_ok(channel:: AMQP.Channel.t(), meta :: map) :: :ok | no_return
+  @callback basic_cancel(channel:: AMQP.Channel.t(), meta :: map) :: :ok | no_return
+  @callback basic_cancel_ok(channel:: AMQP.Channel.t(), meta :: map) :: :ok | no_return
 
   defmacro __using__(_opts) do
     quote do
@@ -19,10 +19,10 @@ defmodule Linklab.Bunny.Consumer do
 
       def setup(_, _config), do: :ok
       def basic_deliver(_, _, _), do: :ok
+      def basic_return(_, _, _), do: :ok
       def basic_consume_ok(_, _), do: :ok
       def basic_cancel(_, _), do: :ok
       def basic_cancel_ok(_, _), do: :ok
-      def basic_return(_, _, _), do: :ok
 
       defoverridable Linklab.Bunny.Consumer
     end
