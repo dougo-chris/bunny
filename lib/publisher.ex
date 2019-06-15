b2cbc65ac5f01bc282fda899cab54ae0a00d44d8
Chris Douglas <dougo.chris@gmail.com>
2019-06-15 11:04:56 +1000

    publish/retry to the current topic

--- lib/publisher.ex
+++ lib/publisher.ex
@@ -10,4 +10,26 @@ defmodule Linklab.Bunny.Publisher do
       AMQPBasic.publish(channel, exchange, routing_key, payload, options)
     end)
   end
+
+  @spec publish(atom, any, list) :: :ok | AMQP.Basic.error()
+  def publish(channel_name, payload, options \\ []) do
+    BunnyPool.with_channel(channel_name, fn
+      channel, %{exchange: exchange, topic: topic} ->
+        AMQPBasic.publish(channel, exchange, topic, payload, options)
+
+      channel, %{exchange: exchange} ->
+        AMQPBasic.publish(channel, exchange, "", payload, options)
+    end)
+  end
+
+  @spec retry(atom, any, list) :: :ok | AMQPBasic.error()
+  def retry(channel_name, payload, options \\ []) do
+    BunnyPool.with_channel(channel_name, fn
+      channel, %{exchange: exchange, topic: topic} ->
+        AMQPBasic.publish(channel, exchange, "#{topic}-retry", payload, options)
+
+      channel, %{exchange: exchange} ->
+        AMQPBasic.publish(channel, exchange, "retry", payload, options)
+    end)
+  end
 end
