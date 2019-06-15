01ed7068292069a84a5a2a6fa13987bdfa297b3a
Chris Douglas <dougo.chris@gmail.com>
2019-06-15 14:23:55 +1000

    publisher uses the topic

--- lib/publisher.ex
+++ lib/publisher.ex
@@ -4,13 +4,6 @@ defmodule Linklab.Bunny.Publisher do
   alias AMQP.Basic, as: AMQPBasic
   alias Linklab.Bunny.Pool, as: BunnyPool
 
-  @spec publish(atom, String.t(), any, list) :: :ok | AMQP.Basic.error()
-  def publish(channel_name, routing_key, payload, options \\ []) do
-    BunnyPool.with_channel(channel_name, fn channel, %{exchange: exchange} ->
-      AMQPBasic.publish(channel, exchange, routing_key, payload, options)
-    end)
-  end
-
   @spec publish(atom, any, list) :: :ok | AMQP.Basic.error()
   def publish(channel_name, payload, options \\ []) do
     BunnyPool.with_channel(channel_name, fn
